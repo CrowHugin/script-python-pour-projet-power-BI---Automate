97745fceff1ee6fc802aa726bb61d3a73f5a6f73
CrowHugin <bastien.kuntz@gmail.com>
2023-11-25 18:37:39 +0100

    rajout du changement d'extension

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from zipfile import ZipFile 
 from urllib import request
+import shutil
 
+#---------Downalod du fichier--------
 URL = "https://eco2mix.rte-france.com/download/eco2mix/eCO2mix_RTE_En-cours-TR.zip"
-
 response = request.urlretrieve(URL, "eCO2mix_RTE_En-Cours-TR.zip")
-
 file = "eCO2mix_RTE_En-Cours-TR.zip"
   
-# ouvrir le fichier zip en mode lecture
+
+#----------extraire le fichier sous format xls--------------
+
 with ZipFile(file, 'r') as zip: 
-    # afficher tout le contenu du fichier zip
-    zip.printdir() 
-  
     # extraire tous les fichiers
     print('extraction...') 
     zip.extractall() 
     print('Terminé!')
 
+
+
+#---------------Copier le fichier sous format xlsx------------------------------
+shutil.copyfile("eCO2mix_RTE_En-cours-TR.xls","donnes.xlsx")
